d63c549492ac02b08381b96da65426d051004f38
Alisha <fmi-pcc-lt-25@FM-PC-LT-157s-MacBook-Pro.local>
2024-06-01 12:46:33 +0545

    Theme setup

---
+++ lib/ui/theme/palette.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  Palette._();
+  static const primary = Color(0xFF007BFF); // Blue
+  static const secondary = Color(0xFFFFA500); // Orange
+  static const success = Color(0xFF28A745); // Green
+  static const error = Color(0xFFDC3545); // Red
+  static const warning = Color(0xFFFFC107); // Yellow
+  static const info =
+      Color(0xFF17A2B8); // Cyan (additional for informational purposes)
+  static const light = Color(0xFFF8F9FA); // Light Gray (app background)
+  static const dark = Color(0xFF343A40); // Dark Gray (primary text)
+  static const background =
+      Color(0xFFFFFFFF); // White (cards and main sections)
+  static const textPrimary = Color(0xFF343A40); // Dark Gray (primary text)
+  static const textSecondary =
+      Color(0xFF6C757D); // Medium Gray (secondary text)
+  static const textDisabled = Color(0xFFADB5BD); // Light Gray (disabled text)
+}
